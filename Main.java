1eb3c77ed78172ff17cc52114d0ef14e74e7d7b2
Guilherme Augusto <augusto.g@outlook.com.br>
2023-08-01 19:24:00 -0300

    funcao checar

--- Main.java
+++ Main.java
@@ -10,8 +10,27 @@ public class Main {
 
             switch (opcao) {
                 case "Conversor de Moeda":
+                	String input = JOptionPane.showInputDialog("Insira o valor: ");
+                	System.out.println(input);
+                	if (check(input)) {
+                		System.out.println("Numero");
+                	}else {
+                		System.out.println("String");
+                	}
+                	break;
+                	             
             }
         }
+    }
+    
+    public static boolean check(String input) {
+    	try {
+    		double x = Double.parseDouble(input);
+    		if (x >= 0 || x < 0);
+    		return true;    		
+    	} catch (NumberFormatException e) {
+    		return false;
+    	}
     }
 
 
