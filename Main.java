52e3f99ff12af1ae5ddea08a6b11ffc21ffc9d08
Guilherme Augusto <augusto.g@outlook.com.br>
2023-08-01 19:11:12 -0300

    caixa de dialogo

---
+++ Main.java
@@ -0,0 +1,18 @@
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        while (true) {
+
+            String opcao = JOptionPane.showInputDialog(null, "Escolha uma opção ", "Menu", JOptionPane.PLAIN_MESSAGE, null, new Object[]{"Conversor de Moeda"}, "Escolha").toString();
+
+            switch (opcao) {
+                case "Conversor de Moeda":
+            }
+        }
+    }
+
+
+}
